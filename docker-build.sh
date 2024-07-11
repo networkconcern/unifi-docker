c3f66e63f6ac00c07b3114f4447cac114bea5eba
Network Concern <synology@networkconcern.com>
2024-07-11 09:41:15 -0400

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -47,6 +47,12 @@ if [ -d "/usr/local/docker/pre_build/$(dpkg --print-architecture)" ]; then
     find "/usr/local/docker/pre_build/$(dpkg --print-architecture)" -type f -exec '{}' \;
 fi
 
+# Fix: Installing specific versions of MongoDB from the official MongoDB repo
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+tryfail apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 656408E390CFB1F5
+apt-get update
+apt-get install -y mongodb-org-server=4.4.6 mongodb-org-shell=4.4.6 mongodb-org-mongos=4.4.6 mongodb-org-tools=4.4.6
+
 curl -L -o ./unifi.deb "${1}"
 apt -qy install ./unifi.deb
 rm -f ./unifi.deb
